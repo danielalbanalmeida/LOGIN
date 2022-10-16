aa4d9fbdff5dd91800f44fe18ff908dddcc454b5
danielalbanalmeida <55764851+danielalbanalmeida@users.noreply.github.com>
2022-10-16 16:42:40 -0500

    Delete login directory

--- login/user/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import *
-
-def feed (request):
-    posts = Post.objects.all()
-
-    context = {'posts':posts}
-    return render (request, 'social/feed.html')
-# Create your views here.
